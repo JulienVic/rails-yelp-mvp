2a3542cbaa47ca05ffd5010c95648c81979e66e0
Julien Victor <victor.julien84@gmail.com>
2020-08-12 16:02:18 +0200

    execice 2 method controller ok and views ok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,20 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to @restaurant
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bristol = Restaurant.create!(name: "Epicure", category: "french", address: "Nantes")
+caristan = Restaurant.create!(name: "Caristan", category: "chinese", address: "Paris")
+pasta = Restaurant.create!(name: "Pasta", category: "italian", address: "Bordeaux")
+fritas = Restaurant.create!(name: "Fritas", category: "belgian", address: "Angers")
+sakura = Restaurant.create!(name: "Sakura", category: "japanese", address: "Orleans")
